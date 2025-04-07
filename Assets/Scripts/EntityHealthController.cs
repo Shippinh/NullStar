af9bbd66063a8228fb9f98b86944576767d06d43
Shippinh <logoshit@gmail.com>
2025-04-07 14:27:37 +0300

    Introduced simple health system

---
+++ Assets/Scripts/EntityHealthController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealthController : MonoBehaviour
+{
+    public int MaxHP = 100;
+    public int CurrentHP = 1;
+    public bool isAlive = true;
+    public bool isInvincible = false;
+    public float invincibilityDuration = 2f;
+    float currentInvincibilityDuration;
+
+    void Awake()
+    {
+        currentInvincibilityDuration = 0f;
+    }
+
+    public void TakeDamage(int takenDamage)
+    {
+        if(isInvincible == false)
+        {
+            isInvincible = true;
+            if(takenDamage > MaxHP)
+            {
+                CurrentHP = 0;
+                return;
+            }
+
+            var newHP = CurrentHP - takenDamage;
+
+            if(newHP <= 0)
+            {
+                CurrentHP = 0;
+            }
+            else
+            {
+                CurrentHP = newHP;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectDeath();
+        HandleInvincibility();
+    }
+
+    private void HandleInvincibility()
+    {
+        if(isInvincible)
+        {
+            currentInvincibilityDuration += Time.deltaTime;
+
+            if(currentInvincibilityDuration >= invincibilityDuration)
+            {
+                isInvincible = false;
+                currentInvincibilityDuration = 0f;
+            }
+        }
+    }
+
+    private void DetectDeath()
+    {
+        if(isAlive && CurrentHP <= 0)
+        {
+            isAlive = false;
+        }
+    }
+}
