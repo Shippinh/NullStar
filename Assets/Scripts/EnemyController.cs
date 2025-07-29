658cb01b7be6ab6c1833e7885c2b2a1d65ab7ec0
Shippinh <logoshit@gmail.com>
2025-07-29 13:13:50 +0300

    introduced simple ramming enemy with basic obstacle avoidance

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class EnemyController : MonoBehaviour
+{
+    public string enemyName = "Default Enemy Name";
+    public EntityHealthController healthControllerRef;
+    // Use this for initialization
+    void Awake()
+    {
+        healthControllerRef = GetComponent<EntityHealthController>();
+        healthControllerRef.Died += HandleEnemyDeath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void HandleEnemyDeath()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
